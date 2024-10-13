0dc83406a4775ff17a2e6fd12815982ea1e51285
Faiyaz Ismail <137979191+Faiyaz-2021331021@users.noreply.github.com>
2024-10-13 19:10:12 +0600

    Create Subarray divisibility.cpp

---
+++ Subarray divisibility.cpp
@@ -0,0 +1,58 @@
+///https://cses.fi/problemset/task/1662/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int             long long
+#define pb              push_back
+#define endl            '\n'
+#define pp              pair<int,pair<int,int>>
+#define asort(a,n)      sort(a,a+n) 
+#define arrout(a,n)     for(int i=0;i<n;i++)cout<<a[i]<<" ";
+#define vcout(v)        for(auto i:v)cout<<i<<" ";
+#define vsort(v)        sort(v.begin(),v.end())
+#define vrsort(v)       sort(v.rbegin(),v.rend());
+#define YES             cout<<"YES"<<endl
+#define NO              cout<<"NO"<<endl
+#define yes             cout<<"yes"<<endl
+#define no              cout<<"no"<<endl
+#define Yes             cout<<"Yes"<<endl
+#define No              cout<<"No"<<endl
+#define pqr             priority_queue<int,vector<int>,greater<int>>
+#define debug           cout<<"HERE"<<endl;
+#define ff              first
+#define ss              second
+void edm()
+{
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+}
+void solve()
+{
+    int n;cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)cin>>arr[i];
+    map<int,int>mp;
+    mp[0] = 1;
+    int sum=0;int ans=0;
+    for(int i=0;i<n;i++)
+    {
+        sum+=arr[i];
+        sum = ((sum%n)+n)%n;
+        ans+=mp[sum];
+        mp[sum]++;
+    }
+    cout<<ans<<endl;
+}
+int32_t main()
+{
+    edm();
+    int t = 1;
+    //cin >> t;
+    for(int i=1;i<=t;i++)
+    {
+        solve();
+    }
+}
